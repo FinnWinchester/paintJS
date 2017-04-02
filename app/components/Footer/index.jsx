98e345cdd360faf767a3b0fe3fcbf8591a777bcc
Finn Winchester <finnwinchester@Finn-Winchester.local>
2017-04-02 12:00:13 +0200

    Fixed footer content

--- app/components/Footer/index.jsx
+++ app/components/Footer/index.jsx
@@ -1,27 +1,41 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 let FooterComponent = () => (
   <footer className="page-footer">
     <div className="container">
       <div className="row">
         <div className="col l6 s12">
-          <h5 className="white-text">Footer Content</h5>
-          <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+          <h5 className="white-text">Documentation</h5>
+          <ul>
+            <li>
+              <a href="https://www.w3schools.com/html/html5_canvas.asp" className="grey-text text-lighten-4">HTML5 canvas</a>
+            </li>
+            <li>
+              <a href="https://facebook.github.io/react/" className="grey-text text-lighten-4">ReactJS</a>
+            </li>
+            <li>
+              <a href="http://redux.js.org/" className="grey-text text-lighten-4">Redux</a>
+            </li>
+            <li>
+              <a href="http://brunch.io/" className="grey-text text-lighten-4">Brunch</a>
+            </li>
+            <li>
+              <a href="https://babeljs.io/" className="grey-text text-lighten-4">Babel/ES6</a>
+            </li>
+            <li>
+              <a href="https://facebook.github.io/jest/" className="grey-text text-lighten-4">Jest</a>
+            </li>
+          </ul>
         </div>
         <div className="col l4 offset-l2 s12">
           <h5 className="white-text">Links</h5>
           <ul>
             <li>
-              <a className="grey-text text-lighten-3" href="#!">Link 1</a>
+              <Link to="/home" className="grey-text text-lighten-3">Home</Link>
             </li>
             <li>
-              <a className="grey-text text-lighten-3" href="#!">Link 2</a>
-            </li>
-            <li>
-              <a className="grey-text text-lighten-3" href="#!">Link 3</a>
-            </li>
-            <li>
-              <a className="grey-text text-lighten-3" href="#!">Link 4</a>
+              <Link to="/paint" className="grey-text text-lighten-3">Paint</Link>
             </li>
           </ul>
         </div>
@@ -29,8 +43,8 @@ let FooterComponent = () => (
     </div>
     <div className="footer-copyright">
       <div className="container">
-        © 2017 Copyright Text
-        <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+        Finn Winchester :: PaintJS
+        <a className="grey-text text-lighten-4 right" href="Fork me on GitHub!">Fork me on GitHub!</a>
       </div>
     </div>
   </footer>
